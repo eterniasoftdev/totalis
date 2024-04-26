71251420966338e2e102abed4159f1b5d6a812b4
Pawan608 <agarwalpawan6089@gmail.com>
2024-04-26 15:52:11 +0530

    totalis complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import StickyNav from "./_components/StickyEnquiry/Page";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +25,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className={`w - screen ${isChecked ? "md:h-screen" : ""}`}>
           <Header setIsChecked={setIsChecked} isChecked={isChecked} />
-          <div className="relative z-30">{children}</div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className="relative z-30">{children}</div>
+          </Suspense>
           <Footer />
           <StickyNav />
           {/* <button className="fixed bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded-md shadow-md z-50">
